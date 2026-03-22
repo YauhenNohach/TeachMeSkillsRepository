01abb51b09168825cd734e4a34ef0ec8bf0bcbfd
Yanina <yanamoroz2000@gmail.com>
2026-03-22 14:18:06 +0300

    hm lesson 7

---
+++ src/yanina_savich/lesson7_interfaces_and_abstract_classes/HM/MammalSecond.java
@@ -0,0 +1,9 @@
+package yanina_savich.lesson7_interfaces_and_abstract_classes.HM;
+
+public class MammalSecond extends PredatorFirst{
+    boolean hasMilk;
+
+    public MammalSecond(String animalName, int age, int weight) {
+        super(animalName, age, weight);
+    }
+}
