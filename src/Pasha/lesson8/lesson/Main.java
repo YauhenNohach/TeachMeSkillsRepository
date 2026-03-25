387cd535be49fa7e2349e74a66cf9ae06b939ebb
Yauhen Nohach Dev <jenaynogach1@mail.ru>
2026-03-25 21:14:00 +0300

    add lesson 9

---
+++ src/Pasha/lesson8/lesson/Main.java
@@ -0,0 +1,20 @@
+package Pasha.lesson8.lesson;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TransferMoney transferMoney = new TransferMoney();
+
+        Bank bank1 = new Bank("bnk", 0.1);
+        Bank bank2 = new Bank("bnk1", 0.2);
+
+        CreditAccount creditAccount = new CreditAccount(120, bank1, 1000);
+        SavingAccount savingAccount = new SavingAccount(1500, bank2);
+
+        transferMoney.transferMoneyFromCreditAccToSavingAcc(creditAccount, savingAccount, bank1);
+
+        System.out.println("Credit acc balance - " + creditAccount.getBalance());
+        System.out.println("Saving acc balance - " + savingAccount.getBalance());
+    }
+
+}
